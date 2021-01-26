56625b6c2a22fd34e505aa18d85adbbe98e482c6
libf <lbf911@126.com>
2021-01-26 17:31:49 +0800

    feat: 策略管理 创建项目

---
+++ src/pages/index/index/views/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">23333</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {},
+  name: 'Index',
+})
+export default class Home extends Vue {}
+</script>
