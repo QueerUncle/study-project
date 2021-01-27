ed2e0688f33ba284cd945a3442255062540a2166
libf <lbf911@126.com>
2021-01-27 15:04:48 +0800

    feat: 策略管理 添加页面

--- src/pages/index/index/views/Index.vue
+++ src/pages/index/index/views/Index.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="home">23333</div>
+  <el-button
+    type="primary"
+    @click="handleOpenModal"
+  >主要按钮</el-button>
+  <EntSelect
+    :entDialog="entDialog"
+    @handleEntSelect="handleEntSelect"
+    v-if="entDialog.visible"
+  />
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import EntSelect from '../components/EntSelect/index.vue';
 
 @Options({
-  components: {},
+  components: {
+    EntSelect,
+  },
   name: 'Index',
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  private entDialog = {
+    singleSelect: true,
+    visible: false,
+  };
+
+  private handleOpenModal(): void {
+    this.entDialog.visible = true;
+  }
+
+  private handleEntSelect(params: EntBackParam) {
+    this.entDialog.visible = params.visible;
+  }
+}
 </script>
